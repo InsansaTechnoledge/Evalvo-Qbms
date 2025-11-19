02bcc8975baed2f35dd69b79324ed1a5954b1b0d
BHADRIKA <bhadrika2226@gmail.com>
2025-11-19 14:04:10 +0530

    Add LogoutModal component and integrate with SideAndNavbar for user logout functionality

--- src/components/AfterAuth/navigation/SideAndNavbar.jsx
+++ src/components/AfterAuth/navigation/SideAndNavbar.jsx
@@ -20,6 +20,7 @@ import logo from '/logo.svg';
 import { School } from "lucide-react";
 import { Link } from "react-router-dom";
 import { SideBarData, SideBarSupportData } from "../../../utils/Constants";
+import LogoutModal from "../LogoutModal";
 
 
 function classNames(...xs) {
@@ -172,7 +173,7 @@ const SidebarHeading = ({ children }) => (
 );
 const SidebarSpacer = () => <div className="h-2" />;
 
-const SidebarItem = ({ href , children }) => (
+const SidebarItem = ({ href, children }) => (
   <Link
     to={href}
     className="flex items-center gap-2 rounded-md px-2.5 py-2 text-sm text-zinc-700 hover:bg-white hover:shadow-sm "
@@ -186,188 +187,202 @@ const SidebarItem = ({ href , children }) => (
 const SidebarLabel = ({ children }) => <span className="truncate">{children}</span>;
 
 
-export default function SideAndNavbar({children}) {
+export default function SideAndNavbar({ children }) {
+  const [showLogoutModal, setShowLogoutModal] = useState(false);
   return (
-    <SidebarLayout
-      navbar={
-        <Navbar>
-          <img src={logo} className="w-12 h-12" alt="" /><span className="text-2xl font-bold bg-linear-to-r from-blue-600 to-teal-500 bg-clip-text text-transparent">QBMS</span>
-          <NavbarSpacer />
-          <NavbarSection>
-            <NavbarItem href="/search" ariaLabel="Search">
-              <MagnifyingGlassIcon className="h-5 w-5" />
-            </NavbarItem>
-            <NavbarItem href="/inbox" ariaLabel="Inbox">
-              <InboxIcon className="h-5 w-5" />
-            </NavbarItem>
+    <>
+      {showLogoutModal && <LogoutModal setShowLogoutModal={setShowLogoutModal} />}
+      <SidebarLayout
+        navbar={
+          <Navbar>
+            <img src={logo} className="w-12 h-12" alt="" /><span className="text-2xl font-bold bg-linear-to-r from-blue-600 to-teal-500 bg-clip-text text-transparent">QBMS</span>
+            <NavbarSpacer />
+            <NavbarSection>
+              <NavbarItem href="/search" ariaLabel="Search">
+                <MagnifyingGlassIcon className="h-5 w-5" />
+              </NavbarItem>
+              <NavbarItem href="/inbox" ariaLabel="Inbox">
+                <InboxIcon className="h-5 w-5" />
+              </NavbarItem>
 
-            {/* Profile dropdown */}
-            <Dropdown
-              button={<Avatar src="https://i.pravatar.cc/80?img=11" square />}
-              anchor="bottom end"
-            >
-              {({ close }) => (
-                <>
-                  <DropdownItem href="/my-profile" icon={UserIcon} onClick={close}>
-                    My profile
-                  </DropdownItem>
-                  <DropdownItem href="/settings" icon={Cog8ToothIcon} onClick={close}>
-                    Settings
-                  </DropdownItem>
-                  <DropdownDivider />
-                  <DropdownItem href="/privacy-policy" icon={ShieldCheckIcon} onClick={close}>
-                    Privacy policy
-                  </DropdownItem>
-                  <DropdownItem href="/share-feedback" icon={LightBulbIcon} onClick={close}>
-                    Share feedback
-                  </DropdownItem>
-                  <DropdownDivider />
-                  <DropdownItem href="/logout" icon={ArrowRightStartOnRectangleIcon} onClick={close}>
-                    Sign out
-                  </DropdownItem>
-                </>
-              )}
-            </Dropdown>
-          </NavbarSection>
-        </Navbar>
-      }
-      sidebar={
-        <Sidebar>
-          {/* Header */}
-          <SidebarHeader>
-            {/* Team switcher */}
-            <Dropdown
-              anchor="bottom start"
-              button={
-                <div className="flex w-full items-center gap-2 rounded-md px-2.5 py-2 hover:bg-zinc-100 ">
-                  <Avatar src={logo} />
-                  <SidebarLabel>Evalvo Qbms</SidebarLabel>
-                  <ChevronDownIcon className="ml-auto h-4 w-4 opacity-70" />
-                </div>
-              }
-              className="min-w-72"
-            >
-              {({ close }) => (
-                <>
-                  <DropdownItem icon={School} onClick={close}>
-                    Schools 
-                  </DropdownItem>
-                  
-                  <DropdownDivider />
-                    
-                  <DropdownItem href="manage-school" onClick={close}>
-                    <span className="flex items-center gap-2">
-                      <Avatar initials="MS" className="bg-purple-500 text-white" />
-                      <span>Manage School</span>
+              {/* Profile dropdown */}
+              <Dropdown
+                button={<Avatar src="https://i.pravatar.cc/80?img=11" square />}
+                anchor="bottom end"
+              >
+                {({ close }) => (
+                  <>
+                    <DropdownItem href="/my-profile" icon={UserIcon} onClick={close}>
+                      My profile
+                    </DropdownItem>
+                    <DropdownItem href="/settings" icon={Cog8ToothIcon} onClick={close}>
+                      Settings
+                    </DropdownItem>
+                    <DropdownDivider />
+                    <DropdownItem href="/privacy-policy" icon={ShieldCheckIcon} onClick={close}>
+                      Privacy policy
+                    </DropdownItem>
+                    <DropdownItem href="/share-feedback" icon={LightBulbIcon} onClick={close}>
+                      Share feedback
+                    </DropdownItem>
+                    <DropdownDivider />
+                    <DropdownItem icon={ArrowRightStartOnRectangleIcon} onClick={() => {
+                        close();
+                        setShowLogoutModal(true);
+                      }}
+                    >
+                      Sign out
+                    </DropdownItem>
+
+                  </>
+                )}
+              </Dropdown>
+            </NavbarSection>
+          </Navbar>
+        }
+        sidebar={
+          <Sidebar>
+            {/* Header */}
+            <SidebarHeader>
+              {/* Team switcher */}
+              <Dropdown
+                anchor="bottom start"
+                button={
+                  <div className="flex w-full items-center gap-2 rounded-md px-2.5 py-2 hover:bg-zinc-100 ">
+                    <Avatar src={logo} />
+                    <SidebarLabel>Evalvo Qbms</SidebarLabel>
+                    <ChevronDownIcon className="ml-auto h-4 w-4 opacity-70" />
+                  </div>
+                }
+                className="min-w-72"
+              >
+                {({ close }) => (
+                  <>
+                    <DropdownItem icon={School} onClick={close}>
+                      Schools
+                    </DropdownItem>
+
+                    <DropdownDivider />
+
+                    <DropdownItem href="manage-school" onClick={close}>
+                      <span className="flex items-center gap-2">
+                        <Avatar initials="MS" className="bg-purple-500 text-white" />
+                        <span>Manage School</span>
+                      </span>
+                    </DropdownItem>
+
+                    <DropdownDivider />
+
+                    <DropdownItem href="add-school" icon={PlusIcon} onClick={close}>
+                      Add New School
+                    </DropdownItem>
+                  </>
+                )}
+              </Dropdown>
+
+
+            </SidebarHeader>
+
+            {/* Body */}
+            <SidebarBody>
+
+              <SidebarSection>
+                <SidebarHeading>Main pages</SidebarHeading>
+                {
+                  SideBarData.map((item, key) => (
+                    <SidebarItem key={key} href={item.href}>
+                      <HomeIcon />
+                      <SidebarLabel>{item.name}</SidebarLabel>
+                    </SidebarItem>
+                  ))
+                }
+              </SidebarSection>
+
+              <SidebarSection className="max-lg:hidden">
+                <SidebarHeading>Support pages</SidebarHeading>
+                {
+                  SideBarSupportData.map((item, key) => (
+                    <SidebarItem key={key} href={item.href}>
+                      <SidebarLabel>{item.name}</SidebarLabel>
+                    </SidebarItem>
+                  ))
+                }
+              </SidebarSection>
+
+              <SidebarSpacer />
+
+              {/* Quick actions */}
+
+              <SidebarSection className="max-lg:hidden">
+                <SidebarItem href="manage-po">
+                  <SidebarLabel>PO ( Program Outcome )</SidebarLabel>
+                </SidebarItem>
+
+                <SidebarItem href="manage-co">
+                  <SidebarLabel>CO ( Course Outcome )</SidebarLabel>
+                </SidebarItem>
+
+                <SidebarItem href="manage-co-po">
+                  <SidebarLabel>CO-PO Maping</SidebarLabel>
+                </SidebarItem>
+              </SidebarSection>
+
+              <SidebarSpacer />
+
+            </SidebarBody>
+
+            {/* Footer */}
+            <SidebarFooter className="max-lg:hidden">
+              <Dropdown
+                anchor="top start"
+                button={
+                  <div className="flex w-full items-center gap-3 rounded-md px-2.5 py-2 hover:bg-zinc-100 ">
+                    <Avatar src="https://i.pravatar.cc/80?img=32" className="h-10 w-10" square alt="" />
+                    <span className="min-w-0">
+                      <span className="block truncate text-sm font-medium text-zinc-950 ">
+                        Erica
+                      </span>
+                      <span className="block truncate text-xs text-zinc-500 ">
+                        erica@example.com
+                      </span>
                     </span>
-                  </DropdownItem>
+                    <ChevronUpIcon className="ml-auto h-4 w-4 opacity-70" />
+                  </div>
+                }
+              >
+                {({ close }) => (
+                  <>
+                    <DropdownItem href="/my-profile" icon={UserIcon} onClick={close}>
+                      My profile
+                    </DropdownItem>
+                    <DropdownItem href="/settings" icon={Cog8ToothIcon} onClick={close}>
+                      Settings
+                    </DropdownItem>
+                    <DropdownDivider />
+                    <DropdownItem href="/privacy-policy" icon={ShieldCheckIcon} onClick={close}>
+                      Privacy policy
+                    </DropdownItem>
+                    <DropdownItem href="/share-feedback" icon={LightBulbIcon} onClick={close}>
+                      Share feedback
+                    </DropdownItem>
+                    <DropdownDivider />
+                    <DropdownItem icon={ArrowRightStartOnRectangleIcon} onClick={() => {
+                        close();
+                        setShowLogoutModal(true);
+                      }}
+                    >
+                      Sign out
+                    </DropdownItem>
 
-                  <DropdownDivider />
-
-                  <DropdownItem href="add-school" icon={PlusIcon} onClick={close}>
-                    Add New School
-                  </DropdownItem>
-                </>
-              )}
-            </Dropdown>
-
-            
-          </SidebarHeader>
-
-          {/* Body */}
-          <SidebarBody>
-
-            <SidebarSection>
-              <SidebarHeading>Main pages</SidebarHeading>
-              {
-                SideBarData.map((item,key) => (
-                  <SidebarItem key={key} href={item.href}>
-                    <HomeIcon/>
-                    <SidebarLabel>{item.name}</SidebarLabel>
-                  </SidebarItem>
-                ))
-              }
-            </SidebarSection>
-
-            <SidebarSection className="max-lg:hidden">
-              <SidebarHeading>Support pages</SidebarHeading>
-              {
-                SideBarSupportData.map((item,key) => (
-                  <SidebarItem key={key} href={item.href}>
-                    <SidebarLabel>{item.name}</SidebarLabel>
-                  </SidebarItem>
-                ))
-              }
-            </SidebarSection>
-
-            <SidebarSpacer />
-
-            {/* Quick actions */}
-
-            <SidebarSection className="max-lg:hidden">
-              <SidebarItem href="manage-po">
-                <SidebarLabel>PO ( Program Outcome )</SidebarLabel>
-              </SidebarItem>
-
-              <SidebarItem href="manage-co">
-                <SidebarLabel>CO ( Course Outcome )</SidebarLabel>
-              </SidebarItem>
-
-              <SidebarItem href="manage-co-po">
-                <SidebarLabel>CO-PO Maping</SidebarLabel>
-              </SidebarItem>
-            </SidebarSection>
-
-            <SidebarSpacer />
-
-          </SidebarBody>
-
-          {/* Footer */}
-          <SidebarFooter className="max-lg:hidden">
-            <Dropdown
-              anchor="top start"
-              button={
-                <div className="flex w-full items-center gap-3 rounded-md px-2.5 py-2 hover:bg-zinc-100 ">
-                  <Avatar src="https://i.pravatar.cc/80?img=32" className="h-10 w-10" square alt="" />
-                  <span className="min-w-0">
-                    <span className="block truncate text-sm font-medium text-zinc-950 ">
-                      Erica
-                    </span>
-                    <span className="block truncate text-xs text-zinc-500 ">
-                      erica@example.com
-                    </span>
-                  </span>
-                  <ChevronUpIcon className="ml-auto h-4 w-4 opacity-70" />
-                </div>
-              }
-            >
-              {({ close }) => (
-                <>
-                  <DropdownItem href="/my-profile" icon={UserIcon} onClick={close}>
-                    My profile
-                  </DropdownItem>
-                  <DropdownItem href="/settings" icon={Cog8ToothIcon} onClick={close}>
-                    Settings
-                  </DropdownItem>
-                  <DropdownDivider />
-                  <DropdownItem href="/privacy-policy" icon={ShieldCheckIcon} onClick={close}>
-                    Privacy policy
-                  </DropdownItem>
-                  <DropdownItem href="/share-feedback" icon={LightBulbIcon} onClick={close}>
-                    Share feedback
-                  </DropdownItem>
-                  <DropdownDivider />
-                  <DropdownItem href="/logout" icon={ArrowRightStartOnRectangleIcon} onClick={close}>
-                    Sign out
-                  </DropdownItem>
-                </>
-              )}
-            </Dropdown>
-          </SidebarFooter>
-        </Sidebar>
-      }
-    >
-      {children}
-    </SidebarLayout>
+                  </>
+                )}
+              </Dropdown>
+            </SidebarFooter>
+          </Sidebar>
+        }
+      >
+        {children}
+      </SidebarLayout>
+    </>
   );
 }
--- src/services/authService.js
+++ src/services/authService.js
@@ -9,3 +9,8 @@ export const checkAuth = async () => {
     const response = await api.get(`/v1/auth/check-auth`);
     return response.data;
 }
+
+export const logout = async () => {
+    const response = await api.post(`/v1/auth/logout`);
+    return response.data;
+};
